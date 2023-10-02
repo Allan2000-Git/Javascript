e0b6a39f6912630fbdfa4daa0c6166effc3de865
Allan Joston Fernandes <allanfernandes459@gmail.com>
2023-10-02 11:45:36 +0530

    without styling

--- Currency-Converter-App/script.js
+++ Currency-Converter-App/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_kpmYE8qWrGT8ksGYLUqtOWJvFLaXo767MSyDma9M';
+const API_URL = 'https://api.currencyapi.com/v3/latest?apikey=cur_live_kpmYE8qWrGT8ksGYLUqtOWJvFLaXo767MSyDma9M&base_currency=';
 
 const inputValue = document.querySelector("#amount");
 const selectValue = document.querySelector("#currency");
@@ -8,6 +8,26 @@ const form = document.getElementById("form");
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
     const amount = parseInt(inputValue.value);
-    const currency = selectValue.value;
-    console.log(amount, currency);
+    const currency = selectValue.value.toUpperCase();
+
+    convertCurrency(currency, amount);
 });
+
+async function convertCurrency(baseCurrency, value) {
+    let myTable = ""; 
+
+    const respose = await fetch(API_URL + baseCurrency);
+    const data = await respose.json();
+
+    for(let obj of Object.keys(data["data"])) {
+        myTable+= `
+        <tr>
+            <td>${obj}</td>
+            <td>${data["data"][obj]["code"]}</td>
+            <td>${Math.round(data["data"][obj]["value"] * value)}</td>
+        </tr>
+        `
+    }
+    const tableBody = document.querySelector("tbody");
+    tableBody.innerHTML = myTable;
+}
